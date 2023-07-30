0c729a06e99f782e208505b960923137aaeb9d40
Dlurak <84224239+Dlurak@users.noreply.github.com>
2023-07-30 21:26:31 +0200

    created the basic markup for the login route

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,21 @@
+<script  lang="ts">
+    let submitButton: HTMLInputElement;
+    let usernameValue: string;
+    let passwordValue: string;
+</script>
+
+<h3>Welcome Back</h3>
+
+<form>
+    <div>
+        <label for="username">Username</label>
+        <input type="text" name="username" bind:value={usernameValue}>
+    </div>
+    <div>
+        <label for="username">Password</label>
+        <input type="password" bind:value={passwordValue}>
+    </div>
+    <input type="submit" value="Login" bind:this={submitButton}>
+</form>
+
+<!-- TODO: Style -->

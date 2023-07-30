603ee577d387dcc50fc13462abf75a2596b9ac5c
Dlurak <84224239+Dlurak@users.noreply.github.com>
2023-07-30 21:51:56 +0200

    implemented that the button gets disabled

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,21 +1,27 @@
-<script  lang="ts">
-    let submitButton: HTMLInputElement;
-    let usernameValue: string;
-    let passwordValue: string;
+<script lang="ts">
+	let submitButton: HTMLInputElement;
+	let usernameValue: string;
+	let passwordValue: string;
+
+	let disabled = false;
+
+    $: {
+        disabled = !(!!usernameValue && !!passwordValue)
+    }
 </script>
 
 <h3>Welcome Back</h3>
 
 <form>
-    <div>
-        <label for="username">Username</label>
-        <input type="text" name="username" bind:value={usernameValue}>
-    </div>
-    <div>
-        <label for="username">Password</label>
-        <input type="password" bind:value={passwordValue}>
-    </div>
-    <input type="submit" value="Login" bind:this={submitButton}>
+	<div>
+		<label for="username">Username</label>
+		<input type="text" name="username" bind:value={usernameValue} />
+	</div>
+	<div>
+		<label for="username">Password</label>
+		<input type="password" bind:value={passwordValue} />
+	</div>
+	<input type="submit" value="Login" bind:this={submitButton} disabled={disabled}>
 </form>
 
 <!-- TODO: Style -->

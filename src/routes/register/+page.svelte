5d87cffa88dcf02abbff1d330a888989f50bc7c2
Dlurak <84224239+Dlurak@users.noreply.github.com>
2023-07-31 21:49:00 +0200

    implemented register functionality

--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -1,6 +1,22 @@
-<script>
+<script lang="ts">
+	import { PUBLIC_API_URL } from '$env/static/public';
 	import LoginInput from '$lib/LoginInput.svelte';
 	import SubmitButton from '$lib/SubmitButton.svelte';
+
+	let username: string;
+	let name: string;
+	let school: string;
+	let className: string;
+	let password: string;
+
+	let disabled = false;
+
+	let errorText = '';
+	let successText = '';
+
+	$: {
+		disabled = !(!!username && !!name && !!school && !!className && !!password); // all fields need to be filled
+	}
 </script>
 
 <svelte:head>
@@ -10,27 +26,78 @@
 <div id="wrapper">
 	<h3>Register now</h3>
 
-	<form>
-		<LoginInput type="text" name="username" tooltip="This is your username, it must be unique" />
-		<LoginInput type="text" name="name" tooltip="This is your name, it doesn't need to be unique" />
+	<form
+		on:submit={() => {
+			fetch(PUBLIC_API_URL + '/auth/register', {
+				method: 'POST',
+				headers: new Headers({ 'content-type': 'application/json' }),
+				body: JSON.stringify({
+					username,
+					name,
+					school,
+					password,
+					class: className
+				})
+			})
+				.then((res) => res.json())
+				.then((obj) => {
+					if (obj.status === 'error') {
+						const errorTextesObj = {
+							[`User ${username} already exists`]: 'The username is already taken',
+							[`School ${school} does not exist`]: "This school doesn't exist",
+							[`Class ${className} does not exist`]: "This class doesn't exist",
+							'Internal server error': 'User could not be created'
+						};
+
+						errorText = errorTextesObj[obj.error] || 'Something went wrong';
+						successText = '';
+					} else if (obj.status === 'success') {
+						successText = 'You are now a user!';
+						errorText = '';
+					} else {
+						errorText = 'This is very unexpected';
+						successText =
+							"Indeed this is so unexpected that I don't even now if a account was created or not";
+					}
+				});
+		}}
+	>
+		<LoginInput
+			type="text"
+			name="username"
+			tooltip="This is your username, it must be unique"
+			bind:value={username}
+		/>
+		<LoginInput
+			type="text"
+			name="name"
+			tooltip="This is your name, it doesn't need to be unique"
+			bind:value={name}
+		/>
 		<LoginInput
 			type="text"
 			name="school"
 			tooltip="This is your school. Insert the uniquename of your school, currently you can't figuere out what this is but soon"
+			bind:value={school}
 		/>
 		<LoginInput
 			type="text"
 			name="class"
 			tooltip="This is your class. Insert the name of your class."
+			bind:value={className}
 		/>
 		<LoginInput
 			type="password"
 			newPassword={true}
 			tooltip="This is your password, currently I don't require a secure password but you should choose a secure one"
+			bind:value={password}
 		/>
 
-        <SubmitButton value="Register" disabled/>
+		<SubmitButton value="Register" {disabled} />
 	</form>
+
+	<p id="successMessage">{successText}</p>
+	<p id="errorMessage">{errorText}</p>
 </div>
 
 <style>
@@ -59,5 +126,18 @@
 	form {
 		display: grid;
 		grid-template-columns: 1fr;
+	}
+
+	/* SUCCESS AND ERROR MESSAGES */
+
+	:is(#successMessage, #errorMessage):empty {
+		display: none;
+	}
+
+	#successMessage {
+		color: var(--success);
+	}
+	#errorMessage {
+		color: var(--error);
 	}
 </style>

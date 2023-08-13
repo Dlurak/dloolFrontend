a4ba3d07a2964842bce582e7804b8df24583bb15
Dlurak <84224239+Dlurak@users.noreply.github.com>
2023-08-13 08:37:58 +0200

    refactored the language switcher and the logo

--- src/lib/LanguageSwitcher.svelte
+++ src/lib/LanguageSwitcher.svelte
@@ -4,22 +4,12 @@
 	let selected: string;
 </script>
 
-<select bind:value={selected} on:change={() => switchLanguage(selected)}>
+<select
+	bind:value={selected}
+	on:change={() => switchLanguage(selected)}
+	class="bg-transparent text-light-text dark:text-dark-text border-none p-1 rounded-md focus:bg-light-secondary dark:focus:bg-dark-secondary hover:bg-light-secondary dark:hover:bg-dark-secondary"
+>
 	{#each languages as lang}
 		<option value={lang} selected={lang === language}>{lang}</option>
 	{/each}
 </select>
-
-<style>
-	select {
-		background-color: transparent;
-		color: var(--text);
-		border: none;
-		padding: 0.2rem;
-		border-radius: 0.25rem;
-	}
-
-	select:is(:focus, :hover) {
-		background-color: var(--secondary);
-	}
-</style>
--- src/lib/Logo.svelte
+++ src/lib/Logo.svelte
@@ -1,13 +1,3 @@
-<a href="/">
-	<img src="/assets/dloolLogo.svg" alt="Logo" title="Dlool" />
+<a href="/" class="h-full">
+	<img src="/assets/dloolLogo.svg" alt="Logo" title="Dlool" class="h-full object-contain" />
 </a>
-
-<style>
-	a {
-		height: 100%;
-	}
-	img {
-		height: 100%;
-		object-fit: contain;
-	}
-</style>

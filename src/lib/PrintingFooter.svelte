f5d24f4287b2207787fd5e2740bffd9fd93b76af
Dlurak <84224239+Dlurak@users.noreply.github.com>
2023-08-15 23:07:18 +0200

    translated the printing footer

--- src/lib/PrintingFooter.svelte
+++ src/lib/PrintingFooter.svelte
@@ -1,6 +1,7 @@
 <script>
 	import { onMount } from 'svelte';
 	import Logo from './Logo.svelte';
+	import { i } from '@inlang/sdk-js';
 
 	let isFirefox = true;
 
@@ -10,14 +11,12 @@
 </script>
 
 {#if !isFirefox}
-	<div
-		class="hidden print:flex h-14 justify-between items-center px-2 outline outline-1 outline-red-200"
-	>
+	<div class="hidden print:flex h-14 justify-between items-center px-2">
 		<div class="flex flex-row justify-start items-center h-full gap-2">
 			<Logo />
 			<h3>Dlool</h3>
 		</div>
 
-		<p class="text-gray-600">Your colloborative homeworkbook</p>
+		<p class="text-gray-600">{i('printing.footer')}</p>
 	</div>
 {/if}

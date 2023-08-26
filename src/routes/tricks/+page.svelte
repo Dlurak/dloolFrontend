8e86bd260a1f44bd0b12ab3cb8e5775f927c7c02
Dlurak <84224239+Dlurak@users.noreply.github.com>
2023-08-26 16:06:46 +0200

    optimized the export section
    implemented support for todo.txt export

--- src/routes/tricks/+page.svelte
+++ src/routes/tricks/+page.svelte
@@ -2,6 +2,9 @@
 	import { PUBLIC_API_URL } from '$env/static/public';
 	import SubmitButton from '$lib/SubmitButton.svelte';
 	import Input from '$lib/auth/Input.svelte';
+	import SelectDataList from '$lib/auth/SelectDataList.svelte';
+	import { loadClasses } from '$lib/auth/loadClasses';
+	import { loadSchools } from '$lib/auth/loadSchools';
 	import Box from '$lib/homework/Box.svelte';
 	import { i } from '@inlang/sdk-js';
 	import { onMount } from 'svelte';
@@ -15,13 +18,15 @@
 
 	let communicationText = '';
 
+	let resultType = 'calendar';
+
 	let disabled = true;
 
 	onMount(() => {
 		clipboardIsAvailable = navigator.clipboard !== undefined;
 	});
 
-	$: resultUrl = `${PUBLIC_API_URL}/homework/calendar/${school}?classes=${classes.join(',')}`;
+	$: resultUrl = `${PUBLIC_API_URL}/homework/${resultType}/${school}?classes=${classes.join(',')}`;
 	$: {
 		disabled = school === '' || classes.some((c) => c === '');
 	}
@@ -31,17 +36,40 @@
 	<title>Dlool | {i('tricks')}</title>
 </svelte:head>
 
-<h2>{i('tricks.ical')}</h2>
+<h2 id="export">{i('tricks.export')}</h2>
 <Box>
+	<div>
+		<select
+			class="bg-transparent w-full rounded-md border-2 border-gray-400 p-4"
+			bind:value={resultType}
+		>
+			<option value="calendar">iCal</option>
+			<option value="todo">Todo.txt</option>
+		</select>
+	</div>
 	<div class="flex flex-col">
 		<div class="flex flex-row gap-2">
 			<div class="flex flex-col justify-start">
-				<Input type="text" name={i('school')} bind:value={school} />
+				<Input
+					type="text"
+					name={i('school')}
+					bind:value={school}
+					autocomplete="off"
+					list="schoolsList"
+				/>
+				<SelectDataList id="schoolsList" loadFunction={loadSchools} searchParam={school} />
 			</div>
 			<div class="flex flex-col">
 				{#each classes as _, index}
-					<Input type="text" name="{i('class')} {index + 1}" bind:value={_} />
+					<Input
+						type="text"
+						name="{i('class')} {index + 1}"
+						bind:value={_}
+						autocomplete="off"
+						list="classList"
+					/>
 				{/each}
+				<SelectDataList id="classList" loadFunction={loadClasses} searchParam={school} />
 				<button
 					on:click={(e) => {
 						e.preventDefault();
@@ -55,11 +83,11 @@
 			<!--The clipboard API is only avialable with a secure origin-->
 			{#if clipboardIsAvailable}
 				<SubmitButton
-					value={i('tricks.ical.copy')}
+					value={i('tricks.export.copy')}
 					onClick={(e) => {
 						e.preventDefault();
 						navigator.clipboard.writeText(resultUrl).then(() => {
-							communicationText = i('tricks.ical.copy.success');
+							communicationText = i('tricks.export.copy.success');
 						});
 					}}
 					{disabled}

d70af9a6fa6b325c6e701346f0f247960f2730ee
Dlurak <84224239+Dlurak@users.noreply.github.com>
2023-08-30 14:06:52 +0200

    fixed a bug

--- src/routes/requests/+page.svelte
+++ src/routes/requests/+page.svelte
@@ -4,6 +4,8 @@
 
 	export let data: RequestResponse;
 
+	let isDataError = data.status === 'error';
+
 	let colorObj: {
 		[key in RequestStatus]: string;
 	} = {
@@ -15,7 +17,7 @@
 </script>
 
 <div class="flex flex-col gap-3">
-	{#if data}
+	{#if data && !isDataError}
 		<h3>{i('request.status.status')}</h3>
 		<div class="font-bold text-4xl capitalize p-7 {color} rounded-xl text-center">
 			{i(`request.status.${data.data?.status}`)}

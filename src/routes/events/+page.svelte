580f2c56636c2eaf796ef22384b8d58907e0cb19
Dlurak <84224239+Dlurak@users.noreply.github.com>
2023-09-25 19:19:51 +0200

    Implement a basic static calendar

--- src/routes/events/+page.svelte
+++ src/routes/events/+page.svelte
@@ -1,8 +1,38 @@
-<div class="w-full h-full flex items-center justify-center">
-	<div class="whitespace-pre-line text-center">
-		This site isn't fully implemented yet There is going to be a classical calendar view here as
-		well as a detail view for each event and a way to create a new event. Simmilar to the <a
-			href="/notes">notes</a
-		>
-	</div>
+<script>
+	const date = new Date();
+
+	const month = date.getMonth();
+	const year = date.getFullYear();
+
+	const daysInMonth = new Date(year, month + 1, 0).getDate();
+	const firstDay = new Date(year, month, 1);
+
+	const firstDayWeekday = firstDay.getDay();
+
+	const weekdays = [
+		'Montag',
+		'Dienstag',
+		'Mittwoch',
+		'Donnerstag',
+		'Freitag',
+		'Samstag',
+		'Sonntag'
+	];
+</script>
+
+<div class="grid grid-cols-7 p-20">
+	{#each weekdays as weekday}
+		<p>{weekday}</p>
+	{/each}
+
+	{#each Array(firstDayWeekday - 1) as _, i}
+		<div />
+	{/each}
+
+	{#each Array(daysInMonth) as _, i}
+		<div class="aspect-square border border-1 border-gray-600 dark:border-gray-300 p-2">
+			{i + 1}
+			<div class="line-clamp-1 rounded-sm bg-light-box dark:bg-dark-box px-2 py-1 empty:hidden" />
+		</div>
+	{/each}
 </div>

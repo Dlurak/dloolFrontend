58a87360d0f04f53d42fca9cc55c66ba2d2552fb
Dlurak <84224239+Dlurak@users.noreply.github.com>
2023-09-29 17:59:15 +0200

    Implement functionality to add events

--- src/lib/dates/DateTimePicker.svelte
+++ src/lib/dates/DateTimePicker.svelte
@@ -6,16 +6,20 @@
 	export let dateString: string = getDateTimeInInputFormat(new Date());
 	export let date: Date = new Date(dateString);
 	export let dateObj: CustomDateTime = createDateTime(date);
+	export let timestamp: number = date.getTime();
 
-	$: {
-		dateString = getDateTimeInInputFormat(date);
+	const updateProperties = () => {
 		date = new Date(dateString);
 		dateObj = createDateTime(date);
-	}
+		timestamp = date.getTime();
+	};
+
+	$: dateString = getDateTimeInInputFormat(date);
 </script>
 
 <input
 	type="datetime-local"
 	bind:value={dateString}
+	on:input={updateProperties}
 	class="w-full rounded-sm text-light-text dark:text-dark-text bg-transparent"
 />
--- src/lib/events/CreateEvent.svelte
+++ src/lib/events/CreateEvent.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { PUBLIC_API_URL } from '$env/static/public';
 	import SubmitButton from '$lib/SubmitButton.svelte';
 	import DateTimePicker from '$lib/dates/DateTimePicker.svelte';
 	import Box from '$lib/homework/Box.svelte';
@@ -7,21 +8,62 @@
 	import type { CustomDateTime } from '../../types/customDate';
 
 	let disabled = true;
-	let titel = '';
+	let title = '';
 	let subject = '';
-	let dateObj: CustomDateTime;
+	let dateObjStart: CustomDateTime;
 	let description = '';
 	let school = '';
 	let className = '';
 	let location = '';
 
-	$: disabled = !titel || !subject || !dateObj || !description || !school || !className;
+	let startTimestamp: number;
+	let endTimestamp: number;
+
+	$: disabled =
+		!title ||
+		!subject ||
+		!dateObjStart ||
+		!description ||
+		!school ||
+		!className ||
+		!(startTimestamp < endTimestamp);
+
+	const handleSubmit = (e: SubmitEvent) => {
+		e.preventDefault();
+		const durationMilliSeconds = endTimestamp - startTimestamp;
+		const durationSeconds = durationMilliSeconds / 1000;
+
+		const eventObj: any = {
+			title,
+			description,
+			date: dateObjStart,
+			duration: durationSeconds,
+			subject,
+			school,
+			class: className
+		};
+		const isLocation = !!location;
+		if (isLocation) eventObj.location = location;
+
+		const token = localStorage.getItem('token') as string;
+		const res = fetch(`${PUBLIC_API_URL}/events`, {
+			method: 'POST',
+			body: JSON.stringify(eventObj),
+			headers: {
+				Authorization: `Bearer ${token}`,
+				'Content-Type': 'application/json'
+			}
+		});
+
+		console.log(res);
+		res.then(console.log);
+	};
 </script>
 
 <Box>
-	<div class="flex flex-col gap-2">
+	<form class="flex flex-col gap-2" on:submit={handleSubmit}>
 		<h3 class="grid grid-cols-1 md:grid-cols-2 gap-2">
-			<NormalInput bind:value={titel} placeholder="Titel" />
+			<NormalInput bind:value={title} placeholder="Titel" />
 			<NormalInput bind:value={subject} placeholder="Fach" />
 		</h3>
 		<NormalInput type="textarea" bind:value={description} placeholder="Beschreibung" />
@@ -31,7 +73,10 @@
 			<SchoolClassInput type="class" searchParam={school} bind:value={className} />
 		</div>
 		<NormalInput bind:value={location} placeholder="Location" />
-		<DateTimePicker bind:dateObj />
-		<SubmitButton {disabled} value="Not working yet" />
-	</div>
+
+		<DateTimePicker bind:dateObj={dateObjStart} bind:timestamp={startTimestamp} />
+		<DateTimePicker bind:timestamp={endTimestamp} />
+
+		<SubmitButton {disabled} value="Submit" />
+	</form>
 </Box>

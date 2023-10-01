2d9436d3d9cc81a3b12b5d33dd3cb32a9bf5acde
Dlurak <84224239+Dlurak@users.noreply.github.com>
2023-10-01 16:20:22 +0200

    Remove an unnecessary giant comment

--- src/lib/dates/EndStartDateTiimePicker.svelte
+++ src/lib/dates/EndStartDateTiimePicker.svelte
@@ -1,32 +1,3 @@
-<!-- <script lang="ts">
-	import type { CustomDateTime } from '../../types/customDate';
-	import { createDateTime } from './createDateObject';
-	import { getDateTimeInInputFormat } from './getDateInInputFormat';
-
-	export let dateString: string = getDateTimeInInputFormat(new Date());
-	export let date: Date = new Date(dateString);
-	export let dateObj: CustomDateTime = createDateTime(date);
-	export let timestamp: number = date.getTime();
-
-	const updateProperties = () => {
-		date = new Date(dateString + 'Z');
-		dateObj = createDateTime(date);
-		timestamp = date.getTime();
-	};
-
-	$: dateString = getDateTimeInInputFormat(date);
-</script>
-
-<input
-	type="datetime-local"
-	bind:value={dateString}
-	on:input={updateProperties}
-	class="w-full rounded-sm text-light-text dark:text-dark-text bg-transparent"
-/>
-<p>{dateString}Z</p>
-<p>{new Date(dateString + 'Z')}</p>
-<p>{date}</p> -->
-
 <script lang="ts">
 	let startString = '';
 	export let startDate: Date = new Date();

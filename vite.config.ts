c55cfb07045616cd00e8ff0c2adcf3cbd17b184e
Dlurak <84224239+Dlurak@users.noreply.github.com>
2023-10-05 16:50:10 +0200

    Implement offline support

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,30 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [
+		sveltekit(),
+		VitePWA({
+			manifestFilename: 'manifest.json',
+			workbox: {
+				runtimeCaching: [
+					{
+						// Dlool-backend
+						// ! When using a different backend it isn't enough to change the .env also change it here
+						urlPattern: ({ url }) =>
+							url.toString().startsWith('https://dlool-backend.onrender.com'),
+						handler: 'NetworkFirst',
+						method: 'GET',
+						options: {
+							cacheName: 'backend',
+							expiration: {
+								maxAgeSeconds: 60 * 60 * 24 * 7
+							}
+						}
+					}
+				]
+			}
+		})
+	]
 });

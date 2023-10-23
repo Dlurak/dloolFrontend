e7be25e29dcb0dfe19e9c7466e3a99c6e2c60b3d
Dlurak <84224239+Dlurak@users.noreply.github.com>
2023-10-23 22:45:25 +0200

    Add an indicator which route is selected

--- src/lib/navbar/Navbar.svelte
+++ src/lib/navbar/Navbar.svelte
@@ -1,8 +1,17 @@
 <script lang="ts">
+	import { page } from '$app/stores';
 	import { navData } from '../../constants/nav';
 	import { i } from '../../languages/i18n';
 
 	export let height;
+
+	const navDataEntries = navData.filter((navDataEntry) => navDataEntry.showInNav);
+
+	let currentUri = $page.url.pathname as string;
+
+	page.subscribe(() => {
+		currentUri = $page.url.pathname;
+	});
 </script>
 
 <div
@@ -18,19 +27,38 @@
 		<div
 			class="flex justify-around items-center gap-4 sm:gap-8 w-full sm:w-auto flex-wrap sm:flex-nowrap"
 		>
-			{#each navData as navDataEntry}
-				{#if navDataEntry.showInNav}
-					<a
-						class="flex flex-col justify-center items-center text-light-text dark:text-dark-text cursor-pointer relative after:absolute after:left-0 after:h-[2px] after:bg-light-text after:dark:bg-dark-text after:bottom-0 after:w-0 hover:after:w-full focus:after:w-full after:transition-all after:duration-300 after:ease-in-out focus:outline-none py-1"
-						href={navDataEntry.uri}
-					>
-						<i class="bx {navDataEntry.navBoxIcon} text-lg" />
-						<span class="text-xs">
-							{i(`nav.${navDataEntry.title}`, {}, { transform: 'capitalize' })}
-						</span>
-					</a>
-				{/if}
+			{#each navDataEntries as navDataEntry}
+				<a
+					class="flex flex-col justify-center items-center text-light-text dark:text-dark-text cursor-pointer relative after:absolute after:left-0 after:h-[2px] after:bg-light-text after:dark:bg-dark-text after:bottom-0 after:w-0 hover:after:w-full focus:after:w-full after:transition-all after:duration-300 after:ease-in-out focus:outline-none py-1"
+					href={navDataEntry.uri}
+				>
+					<div class="relative" class:active={currentUri.startsWith(navDataEntry.uri)}>
+						<i class="bx {navDataEntry.navBoxIcon} text-lg z-10" />
+					</div>
+					<span class="text-xs">
+						{i(`nav.${navDataEntry.title}`, {}, { transform: 'capitalize' })}
+					</span>
+				</a>
 			{/each}
 		</div>
 	</nav>
 </div>
+
+<style>
+	.active::before {
+		content: '';
+
+		position: absolute;
+		top: 50%;
+		right: 50%;
+		transform: translate(50%, -50%);
+		z-index: -1;
+
+		border-radius: 100vmax;
+
+		height: 125%;
+		aspect-ratio: 1;
+
+		background-color: var(--secondary);
+	}
+</style>

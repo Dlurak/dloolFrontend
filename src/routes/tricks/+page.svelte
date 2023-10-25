008da1fe4ad0d885744d418c3af3ac39e815b0e1
Dlurak <84224239+Dlurak@users.noreply.github.com>
2023-10-25 14:06:00 +0200

    Refactor the tricks

--- src/routes/tricks/+page.svelte
+++ src/routes/tricks/+page.svelte
@@ -1,114 +1,14 @@
 <script lang="ts">
-	import { PUBLIC_API_URL } from '$env/static/public';
 	import I18n from '$lib/I18n.svelte';
-	import SubmitButton from '$lib/SubmitButton.svelte';
-	import Input from '$lib/auth/Input.svelte';
-	import SelectDataList from '$lib/auth/SelectDataList.svelte';
-	import { loadClasses } from '$lib/auth/loadClasses';
-	import { loadSchools } from '$lib/auth/loadSchools';
-	import Box from '$lib/homework/Box.svelte';
-	import { onMount } from 'svelte';
-	import { i } from '../../languages/i18n';
 	import { title } from '../stores';
-	import { addToast } from '$lib/toast/addToast';
 	import Download from '$lib/tricks/Download.svelte';
-
-	let school = '';
-	let classes = [''];
-
-	let clipboardIsAvailable = false;
-
-	let resultUrl = '';
-
-	let resultType = 'calendar';
-
-	let disabled = true;
+	import Export from '$lib/tricks/Export.svelte';
 
 	title.set('tricks');
-
-	onMount(() => {
-		clipboardIsAvailable = navigator.clipboard !== undefined;
-	});
-
-	$: resultUrl = `${PUBLIC_API_URL}/homework/${resultType}/${school}?classes=${classes.join(',')}`;
-	$: {
-		disabled = school === '' || classes.some((c) => c === '');
-	}
 </script>
 
 <div class="flex gap-4 flex-col">
-	<Box>
-		<h2 id="export"><I18n key="tricks.export" /></h2>
-		<div>
-			<select
-				class="bg-transparent w-full rounded-md border-2 border-gray-400 p-4"
-				bind:value={resultType}
-			>
-				<option value="calendar">iCal</option>
-				<option value="todo">Todo.txt</option>
-			</select>
-		</div>
-		<div class="flex flex-col">
-			<div class="flex flex-row gap-2">
-				<div class="flex flex-col justify-start">
-					<I18n>
-						<Input
-							type="text"
-							name={i('school')}
-							bind:value={school}
-							autocomplete="off"
-							list="schoolsList"
-						/>
-					</I18n>
-					<SelectDataList id="schoolsList" loadFunction={loadSchools} searchParam={school} />
-				</div>
-				<div class="flex flex-col">
-					{#each classes as _, index}
-						<I18n>
-							<Input
-								type="text"
-								name="{i('class')} {index + 1}"
-								bind:value={_}
-								autocomplete="off"
-								list="classList"
-							/>
-						</I18n>
-					{/each}
-					<SelectDataList id="classList" loadFunction={loadClasses} searchParam={school} />
-					<button
-						on:click={(e) => {
-							e.preventDefault();
-							classes.push('');
-							classes = classes;
-						}}>+</button
-					>
-				</div>
-			</div>
-			<div class="flex flex-row justify-between items-center">
-				<!--The clipboard API is only avialable with a secure origin-->
-				{#if clipboardIsAvailable}
-					<I18n>
-						<SubmitButton
-							value={i('tricks.export.copy')}
-							onClick={(e) => {
-								e.preventDefault();
-								navigator.clipboard.writeText(resultUrl).then(() => {
-									addToast({
-										type: 'success',
-										content: 'tricks.export.copy.success',
-										duration: 5000
-									});
-								});
-							}}
-							{disabled}
-						/>
-					</I18n>
-				{:else}
-					<code class="mt-3">{resultUrl}</code>
-				{/if}
-			</div>
-		</div>
-	</Box>
+	<Export />
 	<hr />
 	<div>
 		<h2 id="install"><I18n key="tricks.install" /></h2>

59da3bef91f4f276582730415fb10b7b3d652ece
Dlurak <84224239+Dlurak@users.noreply.github.com>
2023-10-26 20:23:02 +0200

    Some changes to the action and test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,7 +1,11 @@
 import { test, expect } from '@playwright/test';
 
 test('playwright is configuired correctly', async ({ page }) => {
+	console.log('Starting the test');
+	console.log(page);
 	await page.goto('/');
+	console.log('Page is visited on /');
 
 	await expect(page).toHaveURL('http://127.0.0.1:4173/');
+	console.log('URL is correct');
 });

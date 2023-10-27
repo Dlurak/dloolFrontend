d9b4b7776ef9062f7bdacc6580c27c4fb9bf418b
Dlurak <84224239+Dlurak@users.noreply.github.com>
2023-10-28 01:52:06 +0200

    :sparkles: :construction: :heavy_plus_sign: :poop: Implement the ability to export and import colors in a JSON-file

--- src/routes/settings/colors/+page.svelte
+++ src/routes/settings/colors/+page.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
 	import QuickActionButton from '$lib/QuickActionButton.svelte';
 	import ColorPicker from '$lib/colors/ColorPicker.svelte';
+	import { checkIfValid, parseContent, readFileContent } from '$lib/colors/downloadSubjectColors';
 	import { subjectColors } from '../../stores';
+	import { addToast } from '$lib/toast/addToast';
 
 	let data = $subjectColors;
 
@@ -20,7 +22,76 @@
 
 <div>
 	<section>
-		<h3>Farben für Fächer</h3>
+		<div class="flex justify-between">
+			<h3>Farben für Fächer</h3>
+
+			<div class="flex items-center gap-1">
+				<QuickActionButton
+					iconName="bxs-file-import"
+					color="text-green-500 dark:text-green-400"
+					title="Importiere die Farben von einer JSON-Datei"
+					on:click={() => {
+						if (
+							!confirm(
+								'Bist du sicher, dass du die Farben importieren möchtest? Die aktuellen Farben werden überschrieben.'
+							)
+						)
+							return;
+
+						const input = document.createElement('input');
+						input.type = 'file';
+						input.accept = '.json';
+
+						input.onchange = async () => {
+							const files = input.files;
+							if (!files) return;
+							const file = files[0];
+							if (!file) return;
+
+							const content = await readFileContent(file);
+							const isValid = checkIfValid(content);
+
+							if (!isValid) {
+								addToast({
+									type: 'error',
+									content: 'error',
+									// content: 'toast.colors.import.error',
+									duration: 5000
+								});
+								return;
+							}
+
+							const parsed = parseContent(content);
+							data = parsed;
+							addToast({
+								type: 'success',
+								// content: 'toast.colors.import.success',
+								content: 'class',
+								duration: 5000
+							});
+						};
+
+						input.click();
+					}}
+				/>
+
+				<QuickActionButton
+					iconName="bx-cloud-download"
+					focusedIconName="bxs-cloud-download"
+					color="text-blue-500 dark:text-blue-400"
+					title="Downloade die Farben als JSON"
+					on:click={() => {
+						const dataStr =
+							'data:text/json;charset=utf-8,' + encodeURIComponent(JSON.stringify(data));
+						const downloadAnchorNode = document.createElement('a');
+						downloadAnchorNode.setAttribute('href', dataStr);
+						downloadAnchorNode.download = 'subjectColors.json';
+
+						downloadAnchorNode.click();
+					}}
+				/>
+			</div>
+		</div>
 		<div class="flex flex-col gap-4">
 			{#each data as entry}
 				<div class="flex flex-row gap-2 items-center justify-between">

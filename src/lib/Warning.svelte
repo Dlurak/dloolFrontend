9c3ca90012dd48261ad1fd52a39f81636b7892ec
Dlurak <84224239+Dlurak@users.noreply.github.com>
2023-11-03 16:15:39 +0100

    :recycle: Refactor the warning into an standalone component

---
+++ src/lib/Warning.svelte
@@ -0,0 +1,14 @@
+<div
+	class="border-yellow-600 dark:border-yellow-400 bg-yellow-300 bg-opacity-20 border-2 border-solid rounded-md flex gap-2 items-center px-3 py-2 warning-box"
+>
+	<i class="bx bx-error text-4xl" />
+	<span class="w-full">
+		<slot />
+	</span>
+</div>
+
+<style>
+	.warning-box {
+		box-shadow: 0 0 0.5rem 0 rgb(250, 204, 21);
+	}
+</style>
--- src/routes/settings/api/+page.svelte
+++ src/routes/settings/api/+page.svelte
@@ -1,5 +1,6 @@
 <script>
 	import I18n from '$lib/I18n.svelte';
+	import Warning from '$lib/Warning.svelte';
 	import Api from '$lib/preferences/Api.svelte';
 </script>
 
@@ -8,23 +9,12 @@
 		<div>
 			<h3><I18n key="settings.api" /></h3>
 			<div class="p-3">
-				<div
-					class="border-yellow-600 dark:border-yellow-400 bg-yellow-300 bg-opacity-20 border-2 border-solid rounded-md flex gap-2 items-center px-3 py-2 warning-box"
-				>
-					<i class="bx bx-error text-4xl" />
-					<span>
-						<I18n key="settings.api.warning" />
-					</span>
-				</div>
+				<Warning>
+					<I18n key="settings.api.warning" />
+				</Warning>
 			</div>
 		</div>
 
 		<Api />
 	</section>
 </div>
-
-<style>
-	.warning-box {
-		box-shadow: 0 0 0.5rem 0 rgb(250, 204, 21);
-	}
-</style>
